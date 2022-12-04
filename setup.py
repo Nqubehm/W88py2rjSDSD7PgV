b5ba8d2537fe71fff883777f71c909be972d2ef8
Lungile Mngonyama <gamedzenonny@gmail.com>
2022-12-04 17:23:46 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+  name='Potential Talents-V4',
+  version='1.0.0',
+  description='Predict potential talents based on calculated fitness scores using jaccard and cosine similarities.',
+  author='Lungile Mngonyama',
+  author_email='lungile1201@gmail.com',
+  url='https://github.com/Nqubehm/W88py2rjSDSD7PgV',
+  packages=find_packages()
+)

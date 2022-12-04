494acbd947a49cb038cedf09888740216485960b
Lungile Mngonyama <gamedzenonny@gmail.com>
2022-12-04 17:13:02 +0200

    Create make_dataset.py

---
+++ src/data/make_dataset.py
@@ -0,0 +1,4 @@
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+# import data
+data = pd.read_csv('../input/seeking-human-resources/potential-talents - Aspiring human resources - seeking human resources.csv')
